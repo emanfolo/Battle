8eb4c14583d439c56511ae5c11292a1ed21fd03f
Emmanuel Folorunsho <emmanuelfolorunsho@icloud.com>
2021-10-06 17:40:06 +0100

    skinny controller

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,15 @@
 class Game 
 
-  #attr_reader :player_1, :player_2
-
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
   end
 
   def player_1
-    @players.first
+    @players[0]
   end
 
   def player_2
-    @players.last
+    @players[1]
   end
 
   def attack(player)
